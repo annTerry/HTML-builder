10b25db7a18450669c0797fce7c86664d379e594
Anna <89348633+annTerry@users.noreply.github.com>
2023-05-04 23:23:15 +0300

    feat: 02-write-file done

---
+++ 02-write-file/index.js
@@ -0,0 +1,39 @@
+const { error } = require('node:console');
+const fs = require('node:fs');
+const path = require('node:path');
+const process = require('node:process');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin
+});
+
+const filePath = path.join(__dirname, 'text.txt');
+
+const simpleError = (error) => {
+    if (error) {
+        console.log(error);
+    }
+}
+
+fs.writeFile(filePath, '', simpleError);
+
+console.log('\x1b[32mInput text for file, Ctrl+C or single word exit on string for finish\x1b[0m');
+
+rl.on('line', (input) => {  
+  if (input === 'exit') {
+    rl.close();     
+  } else {    
+    fs.appendFile(filePath, `${input}'\n'`, simpleError);  
+  }
+});
+
+process.on('SIGINT', function() {
+    let inputString = rl.line;
+    if (inputString !== 'exit') fs.appendFile(filePath, inputString, simpleError);
+    process.exit();
+});
+
+process.on('exit', () => {
+  console.log('\n\x1b[36mThis is the end. Buy!\x1b[0m');
+});
