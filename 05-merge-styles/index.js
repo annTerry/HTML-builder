4aac95b5f73426ee0c6f30cce73980eddd8d5e0a
Anna <89348633+annTerry@users.noreply.github.com>
2023-05-05 00:55:05 +0300

    feat: 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs/promises');
+const path = require('node:path');
+
+const sourceFolder = path.join(__dirname, 'styles');
+const targetFolder = path.join(__dirname, 'project-dist');
+const targetFile = path.join(targetFolder, 'bundle.css');
+
+const createBundle = async (source) => {
+    const files = await fs.readdir(source, { withFileTypes: true });
+    fs.writeFile(targetFile, '');
+    for (const file of files) {
+        if (file.isFile()) {
+            const ext = path.extname(file.name);
+            if (ext.toLowerCase() === '.css') {
+                const fileToRead = await fs.readFile(path.join(sourceFolder, file.name), { encoding: 'utf-8' });
+                if (fileToRead) {
+                    await fs.appendFile(targetFile, fileToRead);
+                }
+            }
+        }
+    }
+}
+
+(async () => {
+    await createBundle(sourceFolder);
+    console.log('\x1b[32mCSS Bundle is created\x1b[0m');
+})();
